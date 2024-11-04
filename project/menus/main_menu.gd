80a33cd9314f6c35888e24e7e453f7073bf6435d
Ethan Hooper <emhooper@bsu.edu>
2024-11-04 14:55:15 -0500

    Add functionality to start button

--- project/menus/main_menu.gd
+++ project/menus/main_menu.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
 
-func _on_button_pressed() -> void:
-	pass # Replace with function body.
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://world/world.tscn")
+
+func _on_exit_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://world/world.tscn")
