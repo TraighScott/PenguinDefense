39a6a6f8a564172057425330b3832aaa3c1c6818
hooperethan123 <emhooper@bsu.edu>
2024-11-04 23:47:34 -0500

    Add Win Condition

--- project/menus/main_menu.gd
+++ project/menus/main_menu.gd
@@ -4,5 +4,6 @@ extends Node2D
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://world/world.tscn")
 
+
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
--- project/world/world.gd
+++ project/world/world.gd
@@ -13,6 +13,8 @@ var tower_limit := 0
 func _physics_process(_delta: float) -> void:
 	if fortress.health == 0:
 		get_tree().change_scene_to_file("res://menus/end_menu.tscn")
+	$FortressHealth.text = "Fortress Health %d/3" % fortress.health
+		
 	if Input.is_action_just_pressed("spawn_tower"):
 		if tower_limit < 5:
 			var mouse_pos := get_global_mouse_position()
@@ -20,6 +22,9 @@ func _physics_process(_delta: float) -> void:
 			get_node("Towers").add_child(tower)
 			tower.position = mouse_pos
 			tower_limit += 1
+			$PlaceableTowers.text = "Placeable Towers: %d" % (5 - tower_limit)
+			
+	$RemainingTime.text = "Remaining Time: %d" % $GameEndTimer.time_left
 
 
 func _on_enemy_spawn_timer_timeout():
@@ -28,3 +33,7 @@ func _on_enemy_spawn_timer_timeout():
 	#print(enemy.enemy)
 	enemy_spawned.emit(enemy.enemy)
 	print("spawned")
+
+
+func _on_game_end_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://menus/win_menu.tscn")
